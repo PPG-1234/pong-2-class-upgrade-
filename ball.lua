81a31ce3e29d37f23df9f29c69d966fc5d4ef292
PPG-1234 <piyushkarmakar102@gmail.com>
2026-04-06 11:09:22 +0530

    Update ball.lua

--- ball.lua
+++ ball.lua
@@ -1,9 +1,9 @@
-local ball={}
-directions={-1,1}
-ball.__index=ball
---making the ball and updates
-function ball:new(x, y, dx, dy)
-    local self=setmetatable({}, ball)
+local Ball={}
+local directions={-1,1}
+Ball.__index=Ball
+--making the Ball and updates
+function Ball:new(x, y, dx, dy)
+    local self=setmetatable({}, Ball)
     self.w=25
     self.h=25
     self.x=x
@@ -12,12 +12,12 @@ function ball:new(x, y, dx, dy)
     self.dy=dy
     return self
 end
-function ball:update(dt)
+function Ball:update(dt)
     self.x=self.x+self.dx*dt
     self.y=self.y+self.dy*dt
 end
 --sets limits up and down
-function ball:limits()
+function Ball:limits()
     if self.y<0 then
         self.y=0
         self.dy=-self.dy
@@ -26,11 +26,11 @@ function ball:limits()
         self.dy=-self.dy
     end
 end
---setting the 
-function ball:reset(dx,dy)
+--setting the ball to the middle of the screen and giving it a random direction
+function Ball:reset(dx,dy)
     self.x=love.graphics.getWidth()/2-12.5
     self.y=love.graphics.getHeight()/2-12.5
     self.dx=dx*directions[math.random(1,2)]
     self.dy=dy*directions[math.random(1,2)]
 end
-return ball
+return Ball
